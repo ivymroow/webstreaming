68732e1020c803bedd63fb62f6da995efb851ed6
ivymroow <ivymroow@users.noreply.github.com>
2026-08-08 17:27:25 -0400

    switch 2embed from .cc to .online

--- server/embeds.js
+++ server/embeds.js
@@ -3,11 +3,11 @@ async function getEmbeds(imdbId, tmdbId, season, episode) {
   const embeds = [];
 
   if (season && episode) {
-    embeds.push({ name: '2Embed', url: `https://www.2embed.cc/embedtv/${id}&s=${season}&e=${episode}` });
+    embeds.push({ name: '2Embed', url: `https://www.2embed.online/embedtv/${id}&s=${season}&e=${episode}` });
     embeds.push({ name: 'MultiEmbed', url: `https://multiembed.mov/?video_id=${id}&s=${season}&e=${episode}` });
     embeds.push({ name: 'Smashy', url: `https://embed.smashystream.com/playere.php?imdb=${id}&s=${season}&e=${episode}` });
   } else {
-    embeds.push({ name: '2Embed', url: `https://www.2embed.cc/embed/${id}` });
+    embeds.push({ name: '2Embed', url: `https://www.2embed.online/embed/${id}` });
     embeds.push({ name: 'MultiEmbed', url: `https://multiembed.mov/?video_id=${id}` });
     embeds.push({ name: 'Smashy', url: `https://embed.smashystream.com/playere.php?imdb=${id}` });
   }
