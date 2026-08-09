7134f15c5523378f0c01bfc47aceb261f97792db
ivymroow <ivymroow@users.noreply.github.com>
2026-08-09 15:02:52 -0400

    add vidsrc.to as embed provider, only working new provider

--- server/embeds.js
+++ server/embeds.js
@@ -1,15 +1,16 @@
 async function getEmbeds(imdbId, tmdbId, season, episode) {
   const embeds = [];
-  const id = imdbId;
 
   if (season && episode) {
-    embeds.push({ name: '2Embed', url: `https://www.2embed.cc/embedtv/${id}&s=${season}&e=${episode}` });
-    embeds.push({ name: 'MultiEmbed', url: `https://multiembed.mov/?video_id=${id}&s=${season}&e=${episode}` });
-    embeds.push({ name: 'Smashy', url: `https://embed.smashystream.com/playere.php?imdb=${id}&s=${season}&e=${episode}` });
+    embeds.push({ name: '2Embed', url: `https://www.2embed.cc/embedtv/${imdbId}&s=${season}&e=${episode}` });
+    if (tmdbId) embeds.push({ name: 'VidSrc', url: `https://vidsrc.to/embed/tv/${tmdbId}/${season}/${episode}` });
+    embeds.push({ name: 'MultiEmbed', url: `https://multiembed.mov/?video_id=${imdbId}&s=${season}&e=${episode}` });
+    embeds.push({ name: 'Smashy', url: `https://embed.smashystream.com/playere.php?imdb=${imdbId}&s=${season}&e=${episode}` });
   } else {
-    embeds.push({ name: '2Embed', url: `https://www.2embed.cc/embed/${id}` });
-    embeds.push({ name: 'MultiEmbed', url: `https://multiembed.mov/?video_id=${id}` });
-    embeds.push({ name: 'Smashy', url: `https://embed.smashystream.com/playere.php?imdb=${id}` });
+    embeds.push({ name: '2Embed', url: `https://www.2embed.cc/embed/${imdbId}` });
+    if (tmdbId) embeds.push({ name: 'VidSrc', url: `https://vidsrc.to/embed/movie/${tmdbId}` });
+    embeds.push({ name: 'MultiEmbed', url: `https://multiembed.mov/?video_id=${imdbId}` });
+    embeds.push({ name: 'Smashy', url: `https://embed.smashystream.com/playere.php?imdb=${imdbId}` });
   }
 
   return embeds.map((e, i) => ({ provider: e.name, embedUrl: e.url, hash: 'embed-' + i, quality: 'HD', fileIndex: 0 }));
