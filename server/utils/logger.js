3cf28015e263384758d13bfb5c9f2a684c89238a
ivymroow <ivymroow@users.noreply.github.com>
2026-08-08 22:06:41 -0400

    add debug to logger - fix keepalive crash every 5min

--- server/utils/logger.js
+++ server/utils/logger.js
@@ -21,4 +21,5 @@ module.exports = {
   info: (message, meta) => write('info', message, meta),
   warn: (message, meta) => write('warn', message, meta),
   error: (message, meta) => write('error', message, meta),
+  debug: (message, meta) => write('debug', message, meta),
 };
