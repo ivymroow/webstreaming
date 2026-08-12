a4694b32bd7f5bd74b621a907e1d4b58dc2c4940
ivymroow <ivymroow@users.noreply.github.com>
2026-08-12 06:29:19 -0400

    fix hash-based detail links - check hash before path in init

--- server/embeds.js
+++ server/embeds.js
@@ -4,14 +4,9 @@ async function getEmbeds(imdbId, tmdbId, season, episode, anilistId) {
   if (season && episode) {
     if (tmdbId) {
       embeds.push({ name: 'VidSrc.to', url: `https://vidsrc.to/embed/tv/${tmdbId}/${season}/${episode}` });
-      if (anilistId) {
-        embeds.push({ name: '2Embed (Sub)', url: `https://www.2embed.cc/embedtv/${imdbId}&s=${season}&e=${episode}` });
-        embeds.push({ name: '2Embed (Dub)', url: `https://www.2embed.cc/embedtv/${imdbId}&s=${season}&e=${episode}&dub=true` });
-      } else {
-        embeds.push({ name: '2Embed', url: `https://www.2embed.cc/embedtv/${imdbId}&s=${season}&e=${episode}` });
-      }
+      embeds.push({ name: 'VidLink', url: `https://vidlink.pro/tv/${tmdbId}/${season}/${episode}` });
     }
-    embeds.push({ name: 'MultiEmbed', url: `https://multiembed.mov/?video_id=${tmdbId||imdbId}&tmdb=1&s=${season}&e=${episode}` });
+    embeds.push({ name: '2Embed', url: `https://www.2embed.cc/embedtv/${imdbId}&s=${season}&e=${episode}` });
     embeds.push({ name: 'Smashy', url: `https://embed.smashystream.com/playere.php?${tmdbId?'tmdb='+tmdbId:'imdb='+imdbId}&s=${season}&e=${episode}` });
   } else {
     if (tmdbId) {
